0e4bf42c1561184fc51c77b1a8450b1d44421ee9
Jonathan <jonathanmoorevla@gmail.com>
2022-07-01 20:42:23 -0500

    commit tree 06ce4f2b7d280556ac711e4b3f9c348770eff0aa

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+const mainRemoved = document.getElementById('main');
+mainRemoved.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = '<p>YOUR-NAME is the champion</p>';
+
+document.body.append(newHeader);
